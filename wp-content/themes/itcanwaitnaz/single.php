f9f08fc17402e6ae2d554cdd55a170e54b062d06
Stacy Mark <stacy@stacymark.com>
2016-03-11 09:14:23 -0800

    hide featured image on Zaadii Tso - show video at top

--- wp-content/themes/itcanwaitnaz/single.php
+++ wp-content/themes/itcanwaitnaz/single.php
@@ -3,7 +3,11 @@
 
 add_action( 'genesis_before_entry', 'featured_post_image', 8 );
 function featured_post_image() {
-  if ( ! is_singular( 'post' ) )  return;
+	// don't show image on following
+	if ( ! is_singular( 'post' ) || is_single('Zaadii Tso'))  {
+  		return;
+  	}
+  	// show images
 	the_post_thumbnail('post-image');
 }
 
